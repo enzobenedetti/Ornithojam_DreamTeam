5680b878e632ee64a4bd67ffba8051fa566d1921
enzobenedetti <enzob210401@gmail.com>
2022-06-03 21:02:47 +0200

    CharacterMovement Done
    not polish

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private float gravity = 10f;
+
+    private float _velocityX;
+    private float _velocityY;
+    private float _velocityZ;
     
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (!controller) controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+        _velocityY -= gravity * Time.deltaTime;
+        controller.Move(new Vector3(_velocityX, _velocityY, _velocityZ) * Time.deltaTime * speed);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 movement = value.Get<Vector2>();
+        Debug.Log(value.Get<Vector2>());
+        _velocityX = movement.x;
+        _velocityZ = movement.y;
+    }
+
+    public void OnJump()
+    {
+        if (controller.isGrounded)
+        {
+            _velocityY = Mathf.Sqrt(jumpForce);
+        }
+    }
+
+    public void OnAction()
     {
         
     }
