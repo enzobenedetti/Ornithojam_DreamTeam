c773ea192baa6c91efe74ee7837d9e1924527154
enzobenedetti <enzob210401@gmail.com>
2022-06-04 11:08:50 +0200

    Better character
    coyote + slope + particle

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,18 +8,24 @@ using UnityEngine.InputSystem;
 public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public ParticleSystem moveParticule;
+    public GameObject jumpParticule;
     [SerializeField] private float speed = 1f;
     [SerializeField] private float holdingSpeed = 6f;
     [SerializeField] private float jumpForce = 1f;
     [SerializeField] private float gravity = 10f;
     [SerializeField] private float holdingStrengh;
     private float _maxFallSpeed = -2f;
+    private float _coyoteTime = 0.1f;
+    private float _timeBalise;
+    private bool _justNotGrounded = true;
 
     private float _velocityX;
     private float _velocityY;
     private float _velocityZ;
 
     private RaycastHit _slopeHit;
+    private RaycastHit _rayHit;
     private float _groundRayDistance = 6f;
     private bool _isSliding;
 
@@ -37,9 +43,6 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         if (InPause) return;
-        if (!controller.isGrounded) _velocityY -= gravity * Time.deltaTime;
-        if (_velocityY < _maxFallSpeed)
-            _velocityY = _maxFallSpeed;
 
         if (OnSteepSlope())
         {
@@ -50,13 +53,50 @@ public class CharacterMovement : MonoBehaviour
         {
             if (_isSliding)
             {
-                _velocityX = 0f;
-                _velocityY = -1f;
-                _velocityZ = 0f;
-                _isSliding = false;
+                if (controller.isGrounded)
+                {
+                    _velocityX = 0f;
+                    _velocityY = -1f;
+                    _velocityZ = 0f;
+                    _isSliding = false;
+                }
+                else
+                {
+                    SteepSlopeMovement();
+                    _velocityX /= 20f;
+                    _velocityZ /= 20f;
+                    _velocityY += _maxFallSpeed * Time.deltaTime;
+                }
             }
         }
+        
+        if (!controller.isGrounded)
+        {
+            _velocityY -= gravity * Time.deltaTime;
+            if (_justNotGrounded)
+            {
+                _timeBalise = Time.time;
+                _justNotGrounded = false;
+            }
+        }
+        else
+        {
+            if (!_justNotGrounded) _justNotGrounded = true;
+        }
 
+        if (!controller.isGrounded ||
+            Vector2.Distance(new Vector2(_velocityX, _velocityZ), Vector2.zero) < 0.1f)
+        {
+            moveParticule.Stop();
+        }
+        else if (!moveParticule.isPlaying)
+        {
+            moveParticule.Play();
+        }
+        
+        if (_velocityY < _maxFallSpeed)
+            _velocityY = _maxFallSpeed;
+        
         if (_isHolding && _cubeHold)
         {
             float holdbuffer = Vector3.Distance(transform.forward.normalized,
@@ -86,9 +126,10 @@ public class CharacterMovement : MonoBehaviour
 
     public void OnJump()
     {
-        if (controller.isGrounded && !_isHolding)
+        if ((controller.isGrounded || Time.time <= _timeBalise + _coyoteTime) && !_isHolding)
         {
             _velocityY = Mathf.Sqrt(jumpForce);
+            Instantiate(jumpParticule, transform.position + Vector3.down/2f, Quaternion.identity);
         }
     }
 
@@ -116,12 +157,20 @@ public class CharacterMovement : MonoBehaviour
     bool OnSteepSlope()
     {
         if (!controller.isGrounded) return false;
-        Debug.DrawRay(transform.position, Vector3.down * _groundRayDistance, Color.red);
-        if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, 
+        if (Physics.SphereCast(transform.position, controller.radius, Vector3.down, out _slopeHit, 
                 (controller.height / 2) + _groundRayDistance))
         {
             float slopeAngle = Vector3.Angle(_slopeHit.normal, Vector3.up);
-            if (slopeAngle > controller.slopeLimit) return true;
+            Debug.Log(slopeAngle);
+            if (Physics.Raycast(transform.position, Vector3.down, out _rayHit,
+                    (controller.height / 2) + _groundRayDistance))
+            {
+                if (Vector3.Dot(_rayHit.normal, _slopeHit.normal) < 0.9f &&
+                    Vector3.Angle(_rayHit.normal, Vector3.up) <= controller.slopeLimit) return false;
+            }
+            else return false;
+
+            if (slopeAngle > controller.slopeLimit && slopeAngle < 89f) return true;
         }
         return false;
     }
@@ -131,6 +180,7 @@ public class CharacterMovement : MonoBehaviour
         Vector3 slopeDirection = Vector3.up - _slopeHit.normal * Vector3.Dot(Vector3.up, _slopeHit.normal);
         float slideSpeed = speed + 2f + Time.deltaTime;
 
+        
         _velocityX = slopeDirection.x * -slideSpeed;
         _velocityZ = slopeDirection.z * -slideSpeed;
         _velocityY -= _slopeHit.point.y;
