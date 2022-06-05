35715a9156492658902e6c7a81e01bb2e156e85e
popote <yvan.bladet@gmail.com>
2022-06-05 17:34:32 +0200

    Push1

--- Assets/Scripts/EndMapTrigger.cs
+++ Assets/Scripts/EndMapTrigger.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndMapTrigger : MonoBehaviour {
+public class EndMapTrigger : MonoBehaviour
+{
+    public int NextMapIndex = 0;
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(NextMapIndex);
         }
     }
 }
